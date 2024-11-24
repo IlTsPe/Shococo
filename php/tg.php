6b0efa1c1db83bcdef1b9bb9ddf86fdf97e3951b
Ilya <110132056+IlTsPe@users.noreply.github.com>
2024-11-24 20:58:23 +0300

    доработка модалок + php файла

--- php/tg.php
+++ php/tg.php
@@ -21,10 +21,14 @@ $name = isset($data['name'])? $data['name']:'';
 $phone = isset($data['phone'])? $data['phone']:'';
 $email = isset($data['email'])? $data['email']:'';
 
+if($email == '') {
+	$txt = urlencode("<b>&#10071; Новая заявка &#10071;</b> \n<b>$fountain</b> \n<b>Количество:</b> $count \n<b>Имя:</b> $name \n<b>Телефон:</b> $phone");
+} else {
+	$txt = urlencode("<b>&#10071; Новая заявка &#10071;</b> \n<b>$fountain</b> \n<b>Количество:</b> $count \n<b>Имя:</b> $name \n<b>Телефон:</b> $phone \n<b>Почта:</b> $email");
+}
+
 $token = "7509910704:AAFiAzx7IQLvwCpxGEwGNSqCcZoGQZZi0qE";
 $chat_id = "-1002495364336";
-
-$txt = urlencode("<b>&#10071; Новая заявка &#10071;</b> \n<b>$fountain</b> \n<b>Количество:</b> $count \n<b>Имя:</b> $name \n<b>Телефон:</b> $phone \n <b>Почта:</b> $email");
 
 $request = "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}";
 $sendToTelegram = fopen($request,"r");
