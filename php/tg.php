b4c72e64f5dd2f261c603a9afd0358d39a74481d
Ilya <110132056+IlTsPe@users.noreply.github.com>
2024-12-01 19:36:45 +0300

    build v1

--- php/tg.php
+++ php/tg.php
@@ -27,8 +27,8 @@ if($email == '') {
 	$txt = urlencode("<b>&#10071; Новая заявка &#10071;</b> \n<b>$fountain</b> \n<b>Количество:</b> $count \n<b>Имя:</b> $name \n<b>Телефон:</b> $phone \n<b>Почта:</b> $email");
 }
 
-$token = "123";
-$chat_id = "123";
+$token = "7509910704:AAFiAzx7IQLvwCpxGEwGNSqCcZoGQZZi0qE";
+$chat_id = "-1002495364336";
 
 $request = "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}";
 $sendToTelegram = fopen($request,"r");
